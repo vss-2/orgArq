aaed2fbb3252803c64517528e3e81a737804da76
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-02-08 15:45:43 -0300

    Barra de menu para o botão sair
    O platform caiu em desuso, mas vou deixar ai pra não esquecer

--- main_Windows.py
+++ main_Windows.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import platform
 from tkinter import *
+from tkinter import Menu
 # OS: Usado para acessar o diretório;
 # DateTime: Usado para saber "momento" (dia, mês, ano);
 # Tkinter: Usado para interface gráfica;
@@ -15,16 +16,19 @@ class Aplicacao:
         self.widget1.pack()
         self.msg = Label(
         self.widget1, text="Em qual diretório quer gerar a pasta?")
+        self.msg["font"] = ("Trebuchet MS", 10)
         self.msg.pack()  # Terminou de usar? Empacota
         self.criar = Button(self.widget1)
         self.criar["text"] = "Gerar pasta"
         self.criar["width"] = 10
+        self.criar["font"] = ("Trebuchet MS", 8)
         self.criar.bind("<Button-1>", self.criarPasta)
         self.criar.pack()
-        self.sair = Button(self.widget1)  # Botão
-        self.sair["text"] = "Sair"  # Texto botão
-        self.sair["width"] = 5  # Comprimento botão
+        self.sair = Button(self.widget1)          # Botão
+        self.sair["text"] = "Sair"                # Texto botão
+        self.sair["width"] = 5                    # Comprimento botão
         self.sair["command"] = self.widget1.quit  # Encerra
+        self.sair["font"] = ("Trebuchet MS", 8)   # Estilo
         self.sair.pack()
 
     def criarPasta(self, event):
@@ -60,5 +64,16 @@ class Aplicacao:
                 print('Criei a pasta do mês:'+mes+'!')
         
 base = Tk()
+base.title("OrgArq")
+
+menu = Menu(base)
+itemMenu = Menu(menu, tearoff = 0)
+# Tearoff é a linha pontilhada
+
+itemMenu.add_command(label="Sair", command = quit)
+#itemMenu.add_separator() Para separar comandos
+
+menu.add_cascade(label="Opções", menu = itemMenu)
+base.config(menu = menu)
 Aplicacao(base)
 base.mainloop()
