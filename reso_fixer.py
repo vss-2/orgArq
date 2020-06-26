51f0112c936fffbede306ec08e12e94cdc0da871
vss-2 <32624576+vss-2@users.noreply.github.com>
2020-06-25 22:09:33 -0300

    Create reso_fixer.py
    Spotify bugfix on Linux Mint: doesnt leave fullscreen without cleaning the settings file

---
+++ reso_fixer.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from os import getcwd, chdir
+
+print('Lembre-se de fechar o Spotify antes de executar!\nLembre-se de dar chmod +x reso_fixer.py')
+
+cwd = os.getcwd()
+if not cwd.endswith('/.config/spotify'):
+	os.chdir(str(Path.home())+'/.config/spotify')
+
+lines = []
+
+with open('prefs2', 'r') as file:
+	lines = file.readlines()
+	c = 0
+
+	for l in lines:
+		if(l.startswith('app.window.position.width=')):
+			print(l)
+			lines[c] = 'app.window.position.width=800\n'
+		elif(l.startswith('app.window.position.height=')):
+			lines[c] = 'app.window.position.width=600\n'
+		c += 1
+	file.close()
+
+with open('prefs2', 'w') as file:
+	file.writelines(lines)
+	file.close()
