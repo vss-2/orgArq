56a7867f529311a0621baaa87eb89e313a07e562
vss-2 <vss-2@github.com>
2021-01-27 21:49:37 -0300

    Menor dependencia entre codigo e valores estaticos
    Substituicao de valores estaticos por posicoes de uma lista, facilitando a manutencao do codigo caso ocorram futuras mudancas na faixa salarial ou nas aliquotas.

--- orgEmp/INSS.py
+++ orgEmp/INSS.py
@@ -2,6 +2,9 @@ from tkinter import *
 from tkinter.font import Font
 from tkinter import messagebox
 
+faixas_salarios = [1100.00, 2203.48, 3305.22, 6433.57]
+faixas_aliquotas = [0.075, 0.09, 0.12, 0.14]
+
 def percentual(salario, inss):
     percentil = inss/salario
     return percential
@@ -25,20 +28,20 @@ def main():
                 messagebox.showerror("Erro de digitação", "Salário inválido!")
                 return
 
-        if(sal < 1045.01):
-            calculo = sal*0.075
-            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/sal)))
-        elif(sal > 1045.00 and sal < 2089.61):
-            calculo = (1045.00*0.075) + ((sal-1045.00)*0.09)
-            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/sal)))
-        elif(sal > 2089.60 and sal < 3134.41):
-            calculo = (1045.00*0.075) + ((2089.60-1045.00)*0.09) + ((sal-2089.60)*0.12)
-            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/sal)))
-        elif(sal > 3134.40 and sal < 6101.06):
-            calculo = (1045.00*0.075) + ((2089.60-1045.00)*0.09) + ((3134.40-1045.00)*0.12) + ((sal-3134.40)*0.14)
-            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/sal)))
+        if(sal < faixas_salarios[0]):
+            calculo = faixas_salarios[0]*faixas_aliquotas[0]
+            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/faixas_salarios[0])))
+        elif(sal > faixas_salarios[0] and sal < faixas_salarios[1]):
+            calculo = (faixas_salarios[0]*faixas_aliquotas[0]) + ((sal-faixas_salarios[0])*faixas_aliquotas[1])
+            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/faixas_salarios[1])))
+        elif(sal > faixas_salarios[1] and sal < faixas_salarios[2]):
+            calculo = (faixas_salarios[0]*faixas_aliquotas[0]) + ((faixas_salarios[1]-faixas_salarios[0])*faixas_aliquotas[1]) + ((faixas_salarios[2]-faixas_salarios[1])*faixas_aliquotas[2])
+            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/faixas_salarios[2])))
+        elif(sal > faixas_salarios[2] and sal < faixas_salarios[3]):
+            calculo = (faixas_salarios[0]*faixas_aliquotas[0]) + ((faixas_salarios[1]-faixas_salarios[0])*faixas_aliquotas[1]) + ((faixas_salarios[2]-faixas_salarios[0])*faixas_aliquotas[2]) + ((sal-faixas_salarios[2])*faixas_aliquotas[3])
+            messagebox.showinfo("INSS para R${:.2f}".format(sal), 'R$ {:.2f} \nAproximadamente {:.2f}%'.format(calculo, (calculo*100/faixas_salarios[3])))
         else:
-	        messagebox.showinfo('Valor alto: R${:.2f}'.format(sal), 'Salário superior a R$ 6101.06, provavelmente é só multiplicar por 14%! \nR${:.2f}'.format(sal*0.14))
+	        messagebox.showinfo('Valor alto: R${:.2f}'.format(sal), 'Salário superior a R${:.2f}, provavelmente é só multiplicar por 14%! \nR${:.2f}'.format(faixas_salarios[3],sal*faixas_aliquotas[3]))
 
     sal_input = StringVar()
     sal_entry = Entry(base, textvariable = sal_input, font=base_fonte)
@@ -48,13 +51,13 @@ def main():
     botao_calcular = Button(base,
         text='Calcular INSS',
         command=calcular,
-	font=base_fonte
+	    font=base_fonte
     )
     botao_calcular.grid(row=1, column=1)
 
     botao_sair = Button(base,
         text='Sair',
-	font=base_fonte,
+	    font=base_fonte,
         command=quit
     )
 
